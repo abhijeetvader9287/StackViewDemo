1f5a7b8488d3da9ba3b2d9c5bc08b089584019d6
abhijeet@twigsoftwares.com <abhijeet@twigsoftwares.com>
2017-05-30 16:33:42 +0530

    first commit

---
+++ StackViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by Twig Soft on 5/30/17.
+//  Copyright © 2017 Twig Soft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.//
+    }
+
+
+}
+
